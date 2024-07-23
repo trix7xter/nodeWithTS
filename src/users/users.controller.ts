2bec07b4f31487d12ba4c7b80cc3fe98d30b8705
eugene pilatov <epilatov@greencode.pro>
2024-07-23 03:18:18 +0300

    add middleware, dto, entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,10 +4,11 @@ import { injectable, inject } from 'inversify';
 import { TYPES } from '../types';
 import { ILogger } from '../logger/logger.interface';
 import 'reflect-metadata';
-import { IUsersController } from './users.controller.interface';
 
-class User {}
-const users = [];
+import { IUsersController } from './users.controller.interface';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { User } from './user.entity';
 
 @injectable()
 export class UsersController extends BaseController implements IUsersController {
@@ -19,12 +20,18 @@ export class UsersController extends BaseController implements IUsersController 
 		]);
 	}
 
-	login(req: Request, res: Response, next: NextFunction): void {
-		users.push(new User());
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
 		this.ok(res, 'login');
 	}
 
-	register(req: Request, res: Response, next: NextFunction): void {
-		this.ok(res, 'register');
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		console.log(body);
+		const newUser = new User(body.email, body.name);
+		await newUser.setPassword(body.password);
+		this.ok(res, newUser);
 	}
 }
